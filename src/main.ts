e18b6a777844d08efa2f6f558e634d46ffcd92f0
nan3ds <sanyanee@3dsinteractive.com>
2020-11-28 14:03:25 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from '@/App'
 import router from '@/router/Index'
 import store from '@/store/Index'
-import '@/plugins/Index'
+import Components from '@/plugins/Index'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Components)
 
 new Vue({
   router,
--- src/plugins/Index.ts
+++ src/plugins/Index.ts
@@ -13,3 +13,5 @@ Object.keys(Components).forEach(name => {
     Vue.component(name, Components[name]);
 });
 
+export default Components
+
