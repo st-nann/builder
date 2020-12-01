b3f61b0083615cb6fe572bf0dcfefa89c0e90d1c
Heart Narongrit <narongrit@3dsinteractive.com>
2020-12-01 18:23:15 +0700

    Add Base.ts

---
+++ src/constants/Base.ts
@@ -0,0 +1,16 @@
+import { IDropdownOption } from '../interfaces/Components'
+
+export const POSITION: IDropdownOption[] = [
+    { label: 'To Left', value: 'left' },
+    { label: 'To Top', value: 'top' },
+    { label: 'To Right', value: 'right' },
+    { label: 'To Bottom', value: 'bottom' }
+]
+
+export const MENU: IDropdownOption[] = [
+    { label: 'Text', value: 'Text', icon: '' },
+    { label: 'Image', value: 'Image', icon: '' },
+    { label: 'Button', value: 'Button', icon: '' },
+    { label: 'Box', value: 'Box' },
+    { label: 'Spacer', value: 'Spacer', icon: '' }
+]
