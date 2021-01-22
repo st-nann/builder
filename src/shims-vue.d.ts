67137044148fa48dccafc6aab95f655549b4db36
nan3ds <sanyanee@3dsinteractive.com>
2021-01-22 18:25:19 +0700

    fix builder

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,5 +2,10 @@ declare module '*' {
   import Vue from 'vue'
   export default Vue
 }
+declare module '*.vue' {
+  import { Component } from 'vue'
+  const component: Component
+  export default component
+}
 
 declare var Quill: any
